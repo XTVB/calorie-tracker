25272b34a1f4822f3a3fa3fbf6e509d417d6aabf
Abdusalam Dihan <abdu.dihan@gmail.com>
2022-03-24 13:25:13 +0100

    Show multiple errors in validation not just the first

--- server/src/utils/validateFoodEntryInput.ts
+++ server/src/utils/validateFoodEntryInput.ts
@@ -4,44 +4,40 @@ import { isDefined } from "./isDefined";
 
 export const validateFoodEntryInput = (options: FoodEntryInput) => {
   const { date, name, calories, price } = options;
+  const errorArray = [];
   if (date > new Date()) {
-    return [
-      {
-        field: "date",
-        message: "Date must be in the past",
-      },
-    ];
+    errorArray.push({
+      field: "date",
+      message: "Date must be in the past",
+    });
   }
 
   if (name.length > 255) {
-    return [
-      {
-        field: "name",
-        message: "Name must have less than 255 characters",
-      },
-    ];
+    errorArray.push({
+      field: "name",
+      message: "Name must have less than 255 characters",
+    });
   }
 
   if (calories <= 0 || calories > 10000) {
-    return [
-      {
-        field: "calories",
-        message: "Calories values must between 1 and 10000",
-      },
-    ];
+    errorArray.push({
+      field: "calories",
+      message: "Calories values must between 1 and 10000",
+    });
   }
 
   if (isDefined(price)) {
     const bigPrice = new BigNumber(price);
-    if (bigPrice.lte(new BigNumber('0.00')) || bigPrice.gt(new BigNumber('10000.00'))) {
-      return [
-        {
-          field: "price",
-          message: "Price must be between 0.01 and 10000.00",
-        },
-      ];
+    if (
+      bigPrice.lte(new BigNumber("0.00")) ||
+      bigPrice.gt(new BigNumber("10000.00"))
+    ) {
+      errorArray.push({
+        field: "price",
+        message: "Price must be between 0.01 and 10000.00",
+      });
     }
   }
 
-  return null;
+  return errorArray.length > 0 ? errorArray : null;
 };
